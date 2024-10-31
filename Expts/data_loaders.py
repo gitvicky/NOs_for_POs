0ced670e720e8ba623232058c33045b4d7429cd2
Mr. Gopakumar <ir-gopa2@login-q-3.data.cluster>
2024-10-31 18:06:20 +0000

    Cleanup and YAMLed for training

--- Expts/data_loaders.py
+++ Expts/data_loaders.py
@@ -21,8 +21,50 @@ def Navier_Stokes_Spectral(n_sims):
     p = data['p'].astype(np.float32)
     x = data['x']
     dt = data['dt']
-
+    
+    dt = torch.tensor(dt, dtype=torch.float)
 
     uv = stacked_fields([u,v,p])[:n_sims]
 
     return uv, x, x, dt
+
+
+def Navier_Stokes_Incomp(n_sims):
+    #PDEBench data
+    data_loc = '/home/ir-gopa2/rds/rds-ukaea-ap001/ir-gopa2/Code/NOs_for_POs/Data'
+    uv = np.load(data_loc + '/NS_incomp_velocity_100_128_128.npy') #uv being the two compnoents of velocity
+    uv = torch.tensor(uv, dtype=torch.float) #Converting to tensor
+    uv = uv.permute(0, 4, 2, 3, 1)[:n_sims]
+    x, y = np.arange(0, 1, 128), np.arange(0, 1, 128) 
+    t = np.arange(0, 5.0, 0.005)
+    dt = 0.005
+    dt = torch.tensor(dt, dtype=torch.float)
+
+    return uv, x, y, dt
+
+
+def JOREK(n_sims):
+    #JOREK MultiBlob Data 
+    #https://iopscience.iop.org/article/10.1088/1741-4326/ad313a/meta
+    data_loc = '/home/ir-gopa2/rds/rds-ukaea-ap001/ir-gopa2/Code/NOs_for_POs/Data'
+    data = data_loc + '/JOREK_filtered.npz' 
+
+    rho = np.load(data)['rho'].astype(np.float32)[:n_sims] / 1e20
+    phi = np.load(data)['Phi'].astype(np.float32)[:n_sims] / 1e5
+    T = np.load(data)['T'].astype(np.float32)[:200][:n_sims] / 1e6
+
+    rho = np.nan_to_num(rho)
+    phi = np.nan_to_num(phi)
+    T = np.nan_to_num(T)
+
+    fields = stacked_fields([rho,phi,T])
+
+    x_grid =  np.load(data)['Rgrid'].astype(np.float32)
+    y_grid =  np.load(data)['Zgrid'].astype(np.float32)
+    t_grid =  np.load(data)['time'].astype(np.float32)
+
+    t_norm = t_grid / t_grid[-1]
+    dt = t_norm[1] - t_norm[0]
+    dt = torch.tensor(dt, dtype=torch.float)
+
+    return fields, x_grid, y_grid, dt
--- Expts/operator_splitting.py
+++ Expts/operator_splitting.py
@@ -1,16 +1,21 @@
-if configuration['Model']['operator splitting']: 
-    #Currently only including the momentum equation 
-    class NO_PO(nn.Module):
-        def __init__(self):
-            super(NO_PO, self).__init__()
-            self.NO_convection = FNO_multi2d(configuration['Data']['t_in'], configuration['Data']['step'], configuration['Model']['modes'], configuration['Model']['modes'], 2, configuration['Model']['width']) 
-            self.NO_diffusion = FNO_multi2d(configuration['Data']['t_in'], configuration['Data']['step'], configuration['Model']['modes'], 2, configuration['Model']['width']) 
-            self.NO_p_grad =  FNO_multi2d(configuration['Data']['t_in'], configuration['Data']['step'], configuration['Model']['modes'], configuration['Model']['modes'], 1, configuration['Model']['width']) 
 
-        def forward(self, vars):
-            uv = vars[:, 0:2]
-            p = vars[:, 2:3]
-            nu = 0.001
-            rhs = - self.NO_convection(uv) + nu*self.NO_diffusion - self.NO_p_grad(p)
+import torch 
+import torch.nn as nn
 
-            return rhs
+from Neural_PDE.Models.FNO import FNO_multi2d
+
+#Currently only including the momentum equation 
+class NS_OS_rhs(nn.Module):#Navier-Stokes Operator-Splitting right-hand-side. 
+    def __init__(self, configuration):
+        super(NS_OS_rhs, self).__init__()
+        self.NO_convection = FNO_multi2d(configuration['Data']['t_in'], configuration['Data']['step'], configuration['Model']['modes'], configuration['Model']['modes'], 2, configuration['Model']['width']) 
+        self.NO_diffusion = FNO_multi2d(configuration['Data']['t_in'], configuration['Data']['step'], configuration['Model']['modes'], 2, configuration['Model']['width']) 
+        self.NO_p_grad =  FNO_multi2d(configuration['Data']['t_in'], configuration['Data']['step'], configuration['Model']['modes'], configuration['Model']['modes'], 1, configuration['Model']['width']) 
+
+    def forward(self, vars):
+        uv = vars[:, 0:2]
+        p = vars[:, 2:3]
+        nu = 0.001
+        rhs = - self.NO_convection(uv) + nu*self.NO_diffusion - self.NO_p_grad(p)
+
+        return rhs
